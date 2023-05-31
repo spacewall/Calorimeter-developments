6fb7b6c4eac0e19266323cf58993eb0ed127524d
Alexander Godov <godovav@oiate.ru>
2023-05-31 15:23:50 +0300

    Исправлены ошибки, добавлена функция отключения каналов

--- calorimeter_adjusting.py
+++ calorimeter_adjusting.py
@@ -1,10 +1,12 @@
 import sys, os
 from pprint import pprint
+import tkinter
 from tkinter.ttk import Frame
 from tkinter.filedialog import askopenfilename
-from tkinter import PhotoImage, Tk, Canvas, ttk, BOTH, RIDGE, Label, RAISED, NO
+from tkinter import PhotoImage, Tk, Canvas, ttk, BOTH, RIDGE, Label, RAISED, NO, Entry
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from scipy.signal import find_peaks, savgol_filter
 
 
@@ -31,7 +33,7 @@ class OutputFrame(Frame):
             relief=RAISED,
             width=1000,
             height=840
-            )
+        )
         self.canvas.pack(side='left', fill=BOTH)
 
         # Разместим изображение схемы калориметра
@@ -42,14 +44,14 @@ class OutputFrame(Frame):
 class AppWindow(Tk):
     """Объект окна Tkinter - окно приложения"""
     # Инициализируем окно
-    def __init__(self):
+    def __init__(self, title: str):
         super().__init__()
 
-        self.title("Юстировка калориметра")
-        self.geometry('1600x2560')
+        self.title(title)
+        # self.geometry("1600x2560")
 
         # self.resizable(width=False, height=False)
-        self.geometry('1438x1000')
+        self.geometry("1438x1000")
 
 
 class BlockSocket:
@@ -71,9 +73,9 @@ class BlockSocket:
         _label.pack(side='top')
 
         # Зададим стиль текста кнопок в панели управления
-        _style = ttk.Style()
-        _style.theme_use('classic')
-        _style.configure(
+        self._style = ttk.Style()
+        self._style.theme_use('classic')
+        self._style.configure(
             'W.TButton',
             foreground='black',
             font=('Artifakt Element', 19)
@@ -119,14 +121,24 @@ class BlockSocket:
             )
         plot_energy_graphs_button.pack(side='top', ipadx=34, ipady=20)
 
+        # Добавим кнопку редактирования исходных данных
+        point_show_button = ttk.Button(
+            window,
+            text="Редактировать данные",
+            width=25,
+            style='W.TButton',
+            command=self.change_input_data_interface
+            )
+        point_show_button.pack(side='top', ipadx=34, ipady=20)
+
         # Создадим стиль таблицы
-        _style.configure(
+        self._style.configure(
             "mystyle.Treeview",
             highlightthickness=0,
             bd=0,
             font=('Artifakt Element', 11)
             )
-        _style.configure(
+        self._style.configure(
             "mystyle.Treeview.Heading",
             font=('Artifakt Element', 13, 'bold')
             )
@@ -152,7 +164,95 @@ class BlockSocket:
 
         self.window.mainloop()
 
+    def change_input_data_interface(self):
+        """
+        Выводит графическое окно для редактирования входных данных, за редактирование не отвечает
+        """
+        self.changer_window = AppWindow("Редактирование данных")
+        self.changer_window.geometry("400x200")
+        self.changer_window.resizable(width=False, height=False)
+
+        try:
+            # Добавим инструкцию
+            _label = Label(
+                self.changer_window,
+                text=f"Введите номера термопар\n через запятую (всего термопар: {self.data.shape[1]})",
+                font=('Artifakt Element', 19),
+                width=26,
+                borderwidth=4
+            )
+
+            # Добавим кнопку редактирования исходных данных
+            data_change_button = ttk.Button(
+                self.changer_window,
+                text="Применить",
+                width=26,
+                style='W.TButton',
+                command=self.change_input_data
+                )
+            
+            # Создадим окно ввода текста
+            self.user_input = Entry(
+                self.changer_window, 
+                font=('Artifakt Element', 19),
+                width=24,
+                background='white',
+                foreground='black'
+                )
+            
+            _label.pack(side='top')
+            self.user_input.pack(side='top')
+            data_change_button.pack(side='top')
+
+            self.changer_window.mainloop()
+        except AttributeError:
+            self.changer_window.destroy()
+            
+            self.error_handler(f"Данные не загружены,\n выберите данные и повторите попытку")
+
+    def error_handler(self, string):
+        """Выводит сообщение об ошибке в графическое окно"""
+        self.changer_window = AppWindow("Ошибка")
+        self.changer_window.geometry("400x100")
+        self.changer_window.resizable(width=False, height=False)
+
+        # Добавим инструкцию
+        _label = Label(
+            self.changer_window,
+            text=string,
+            font=('Artifakt Element', 19),
+            width=28,
+            borderwidth=4
+        )
+
+        _label.pack(side='top')
+        
+        self.changer_window.mainloop()
+
+    def change_input_data(self):
+        """
+        Отвечает за редактирование подгруженных массивов self.data
+        """
+        channels = self.user_input.get().replace(" ", "").split(",")
+        
+        self.changer_window.destroy()
+
+        if "".join(channels).isnumeric():
+            try:
+                self.new_data = self.data
+
+                for channel in frozenset(channels):
+                    # self.new_data = np.delete(self.new_data, int(channel) - 1, 1)
+                    self.data[:, int(channel) - 1] = np.zeros_like(self.data.shape[0])
+            except IndexError:
+                self.error_handler(f"Вы вышли за рамки от 1 до {self.data.shape[1]}")
+        else:
+            self.error_handler("Вы ввели не число, повторите попытку")
+
     def pre_calculations(self) -> int:
+        """
+        Вычисляет точки термодинамического равновесия по исходным данным, возвращает индекс точки попадания пучка
+        """
         # Поиск точки попадания пучка
         max_points_centered = [max(self.data[:, el] - np.dot(np.ones_like(self.data[:, el]), np.mean(self.data[0:20, el]))) for el in range(0, self.data.shape[1])]
         max_point_number = max_points_centered.index(max(max_points_centered)) + 1 # номер термопары
@@ -182,6 +282,7 @@ class BlockSocket:
         return max_point_number
 
     def table_calculations(self, scale_int):
+        """Вычисляет энергии и заполняет таблицу в меню"""
         # Блок анализа производной
         # plt.plot(summary)
         # x = self.data[:, collumn]
@@ -215,6 +316,7 @@ class BlockSocket:
             self.tree.insert("", 'end', values=el)
     
     def energy_dependencies_plots(self):
+        """Вывод энергетических зависимостей в новое окно"""
         # Получим необходимые переменные
         try:
             self.pre_calculations()
@@ -227,6 +329,7 @@ class BlockSocket:
         interval_data = self.data[SLICE_FIELD, :]
         energy_packet = np.zeros(interval_data.shape)
 
+        graphic_window = AppWindow("Энергетические зависимости")
         plt.close()
         fig, (ax_1, ax_2) = plt.subplots(2, 2, figsize=(12, 9))
         
@@ -237,10 +340,13 @@ class BlockSocket:
             energy_packet[:, collumn] = np.dot(delta, 390 * 3 * 0.025)
 
             # ax_1[0].plot(savgol_filter(energies, 111, 5), label=f"Термопара {collumn + 1}")
-            ax_1[0].plot(energies, label=f"Термопара {collumn + 1}")
-            # ax_1[1].plot(np.divide(np.diff(savgol_filter(energies, 550, 5)), np.diff(np.array(range(0, interval_data.shape[0])))))
-            # ax_1[1].plot(savgol_filter(np.diff(energies), 550, 5))
-            ax_1[1].plot(np.diff(energies), label=f"Термопара {collumn + 1}")
+
+            # Условие на удалённые каналы
+            if energies.all(0):
+                ax_1[0].plot(energies, label=f"Термопара {collumn + 1}")
+                # ax_1[1].plot(np.divide(np.diff(savgol_filter(energies, 550, 5)), np.diff(np.array(range(0, interval_data.shape[0])))))
+                # ax_1[1].plot(savgol_filter(np.diff(energies), 550, 5))
+                ax_1[1].plot(np.diff(energies), label=f"Термопара {collumn + 1}")
 
         ax_1[0].plot(np.zeros_like(energies), "--", color="gray")
         ax_1[1].plot(np.zeros_like(energies), "--", color="gray")
@@ -248,8 +354,8 @@ class BlockSocket:
         ax_1[1].set_xlabel("Время, с")
         ax_1[0].set_ylabel("Q, кДж")
         ax_1[1].set_ylabel("dQ, кДж")
-        ax_1[0].set_title("Дифференциальная энергия dQ, переданная калориметру")
-        ax_1[1].set_title("Энергия Q, переданная калориметру")
+        ax_1[0].set_title("Энергия Q, переданная калориметру")
+        ax_1[1].set_title("Дифференциальная энергия dQ, переданная калориметру")
         ax_1[0].legend()
         ax_1[1].legend()
 
@@ -274,9 +380,14 @@ class BlockSocket:
         ax_2[1].set_xlabel("Время, с")
         ax_2[1].set_ylabel("d(∆Q), кДж")
 
-        fig.show()
+        canvas = FigureCanvasTkAgg(fig, graphic_window)
+        canvas.draw()
+        canvas.get_tk_widget().pack(fill='both', expand=100)
+
+        graphic_window.mainloop()
         
     def thermocouples_location(self, max_point_number) -> None:
+        """Строит схему крепления термопар в OutputFrame"""
         # Построим сетку термопар на схеме
         thermocouples = {
             "1": [False, 650, 515],
@@ -325,6 +436,9 @@ class BlockSocket:
                 self.frame.canvas.create_text(x + 15, y + 45, font=('Artifakt Element', 30), text=number, fill="black")
 
     def data_analysis(self, scale_int=560) -> None:
+        """
+        Вызывается по кнопке, заполняет таблицу и строит схему крепления термопар
+        """
         try:
             # Рассчитаем данные для таблицы
             max_point_number = self.pre_calculations()
@@ -346,18 +460,23 @@ class BlockSocket:
             pass
 
     def data_loading(self) -> None:
+        """
+        Вызывается другими функциями или по кнопке, запрашивает файл с данными, создаёт аттрибут data
+        """
         # Запросим директорию файла с данными
         directory = askopenfilename()
 
         # Импортируем данные калориметра
         self.data = np.genfromtxt(directory, delimiter=';', skip_header=True, skip_footer=True)
 
-    def plots(self) -> None:
-        # Построим графики по всем каналам 
+    def plots(self, graphic_window: AppWindow) -> None:
+        """
+        Функция строит зависимости по исходным данным в отдельном окне Tkinter. Важно: окно не очищается - каждый раз строится новое.
+        """
+
         indx = 1
         colors = ['black', 'red', 'blue', 'yellow', 'green', '#8A2BE2', 'pink', 'violet', '#FF4500', '#C71585', '#FF8C00', '#BDB76B', '#4B0082', '#00FF00', '#00FFFF']
-        # Закроем все графические окна с зависимостями
-        plt.close()
+
         fig, ax = plt.subplots(figsize=(10, 5))
 
         for collumn in range(0, self.data.shape[1]):
@@ -375,18 +494,26 @@ class BlockSocket:
         # Выведем графики в окно
         ax.legend()
         ax.set_title("Результаты измерений")
-        fig.show()
+
+        canvas = FigureCanvasTkAgg(fig, graphic_window)
+        canvas.draw()
+        canvas.get_tk_widget().pack(fill='both', expand=100)
+
+        graphic_window.mainloop()
 
     def data_plotting(self) -> None:
+        """Вызывается по кнопке, строит график по исходным данным"""
+        figure = AppWindow("Исходные зависимости")
+
         try:
             # Выведем зависимости в графическое окно
-            self.plots()
+            self.plots(figure)
         except AttributeError:
             # Подгрузим данные
             self.data_loading()
 
             # Выведем зависимости в графическое окно
-            self.plots()
+            self.plots(figure)
         except FileNotFoundError:
             pass
 
@@ -403,7 +530,7 @@ def resource_path(relative_path):
 def main():
     """Запуск программы"""
     # Инициализация рабочей области
-    window = AppWindow()
+    window = AppWindow("Юстировка калориметра")
 
     # Инициализация окна вывода
     frame = OutputFrame(window)
